f0e95a64b55391c789240bd8e4eccb71e02ea7a9
Josiah Purtlebaugh <josiah.purtlebaugh@gmail.com>
2021-12-06 23:55:55 +0900

    Import initial project structure with non-functional title screen

---
+++ Assets/Scripts/HowToPlayHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HowToPlayHandler : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject HowToPlayPanelPrefab;
+
+    GameObject howToPlayPanelObject;
+
+    Canvas parentCanvas;
+    void Start()
+    {
+        if (!HowToPlayPanelPrefab)
+        {
+            Debug.LogError("HowToPlayPanelPrefab undefined! Set HowToPlayPanelPrefab in the inspector.");
+        }
+
+        parentCanvas = GetComponentInParent<Canvas>();
+        if (!parentCanvas)
+        {
+            Debug.LogError("Unable to locate parent Canvas! Make sure this object's parent has a Canvas component.");
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ShowHowToPlayPanel()
+    {
+        if (!HowToPlayPanelPrefab || howToPlayPanelObject)
+            return;
+
+        howToPlayPanelObject = Instantiate(HowToPlayPanelPrefab, parentCanvas.transform);
+    }
+}
